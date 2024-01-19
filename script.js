22fccff9afedb6b2cb8b4090728f442a9ca8b72b
Medve-d <nikolai.zlodeyev@ecole-isitech.fr>
2024-01-19 10:06:05 +0100

    test vidéo

--- script.js
+++ script.js
@@ -17,3 +17,28 @@ const observer = new IntersectionObserver((entries)=> {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el)=> observer.observe(el));
+/* test vidéo */ 
+
+var video = document.getElementById("background-video");
+
+var btn = document.getElementById("btnVideo");
+
+ 
+
+function playAndPause () {
+
+if (video.paused) {
+
+video.play();
+
+btn.innerHTML = "Pause II";
+
+} else {
+
+video.pause();
+
+btn.innerHTML = "Play ▶";
+
+}
+
+}
